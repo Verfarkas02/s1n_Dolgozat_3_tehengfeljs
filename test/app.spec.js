c9a51be18c08ab4ce2fcb4af3f65f81f24e2deab
Molnar Csenge Anna <molnarcsengeanna@ktch.hu>
2023-05-18 14:19:04 +0200

    dolgozat_3MCSA

---
+++ test/app.spec.js
@@ -0,0 +1,20 @@
+/*
+* File: app.spec.js
+* Author: Molnár Csenge Anna
+* Copyright: 2023, Molnár Csenge Anna
+* Group: Szoft I-1 N
+* Date: 2023-05-18
+* Github: 
+* Licenc: GNU GPL
+*/
+describe("Henger felszíne", () => {
+    it('5, 10 magasság, felszín: 471.23889803846896', () => {
+        let act = calcSurface(5, 10);
+        expect(act).toBe(471.23889803846896)
+    });
+
+    it('30, 13 , felszín: 8105.309046261666', () => {
+        let act = calcSurface(30, 13);
+        expect(act).toBe(8105.309046261666)
+    });
+})
---
+++ test/input.spec.js
@@ -0,0 +1,20 @@
+/*
+* File: app.spec.js
+* Author: Molnár Csenge Anna
+* Copyright: 2023, Molnár Csenge Anna
+* Group: Szoft I-1 N
+* Date: 2023-05-18
+* Github: 
+* Licenc: GNU GPL
+*/
+describe('Bemenet input ellenőrzése', () => {
+    it('21 szám  akkor igaz', () => {
+        let act = isGoodInput(21);
+        expect(act).toBe(true);
+    });
+
+    it('abc betű  akkor hamis', () => {
+        let act = isGoodInput('abc');
+        expect(act).toBe(false);
+    });
+});
